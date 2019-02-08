c1f17587d982116fd9ff859c76dd2950ee72a72c
Brandon Berney <brandonberney@protonmail.com>
2019-02-08 10:46:49 -0500

    gui: form layout

---
+++ carpet-estimator/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace carpet_estimator
+{
+    partial class CarpetEstimator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maxWidthLbl = new System.Windows.Forms.Label();
+            this.maxLengthLbl = new System.Windows.Forms.Label();
+            this.carpetRateLbl = new System.Windows.Forms.Label();
+            this.paddingLayersLbl = new System.Windows.Forms.Label();
+            this.firstRoomLbl = new System.Windows.Forms.Label();
+            this.totalCostLbl = new System.Windows.Forms.Label();
+            this.laborCostLbl = new System.Windows.Forms.Label();
+            this.paddingCostLbl = new System.Windows.Forms.Label();
+            this.carpetCostLbl = new System.Windows.Forms.Label();
+            this.yardsNeededLbl = new System.Windows.Forms.Label();
+            this.carpetEstLbl = new System.Windows.Forms.Label();
+            this.firstRoomYes = new System.Windows.Forms.RadioButton();
+            this.firstRoomNo = new System.Windows.Forms.RadioButton();
+            this.totalCalcLbl = new System.Windows.Forms.Label();
+            this.laborCalcLbl = new System.Windows.Forms.Label();
+            this.paddingCalcLbl = new System.Windows.Forms.Label();
+            this.carpetCalcLbl = new System.Windows.Forms.Label();
+            this.yardsCalcLbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.costGroup = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.costGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // maxWidthLbl
+            // 
+            this.maxWidthLbl.AutoSize = true;
+            this.maxWidthLbl.Location = new System.Drawing.Point(37, 64);
+            this.maxWidthLbl.Name = "maxWidthLbl";
+            this.maxWidthLbl.Size = new System.Drawing.Size(85, 13);
+            this.maxWidthLbl.TabIndex = 0;
+            this.maxWidthLbl.Text = "Max Width (feet)";
+            // 
+            // maxLengthLbl
+            // 
+            this.maxLengthLbl.AutoSize = true;
+            this.maxLengthLbl.Location = new System.Drawing.Point(32, 92);
+            this.maxLengthLbl.Name = "maxLengthLbl";
+            this.maxLengthLbl.Size = new System.Drawing.Size(90, 13);
+            this.maxLengthLbl.TabIndex = 1;
+            this.maxLengthLbl.Text = "Max Length (feet)";
+            // 
+            // carpetRateLbl
+            // 
+            this.carpetRateLbl.AutoSize = true;
+            this.carpetRateLbl.Location = new System.Drawing.Point(11, 120);
+            this.carpetRateLbl.Name = "carpetRateLbl";
+            this.carpetRateLbl.Size = new System.Drawing.Size(111, 13);
+            this.carpetRateLbl.TabIndex = 2;
+            this.carpetRateLbl.Text = "Carpet Price (sq. yard)";
+            // 
+            // paddingLayersLbl
+            // 
+            this.paddingLayersLbl.AutoSize = true;
+            this.paddingLayersLbl.Location = new System.Drawing.Point(30, 153);
+            this.paddingLayersLbl.Name = "paddingLayersLbl";
+            this.paddingLayersLbl.Size = new System.Drawing.Size(92, 13);
+            this.paddingLayersLbl.TabIndex = 3;
+            this.paddingLayersLbl.Text = "Layers of Padding";
+            // 
+            // firstRoomLbl
+            // 
+            this.firstRoomLbl.AutoSize = true;
+            this.firstRoomLbl.Location = new System.Drawing.Point(19, 185);
+            this.firstRoomLbl.Name = "firstRoomLbl";
+            this.firstRoomLbl.Size = new System.Drawing.Size(103, 13);
+            this.firstRoomLbl.TabIndex = 4;
+            this.firstRoomLbl.Text = "Is this the first room?";
+            // 
+            // totalCostLbl
+            // 
+            this.totalCostLbl.AutoSize = true;
+            this.totalCostLbl.Location = new System.Drawing.Point(60, 135);
+            this.totalCostLbl.Name = "totalCostLbl";
+            this.totalCostLbl.Size = new System.Drawing.Size(55, 13);
+            this.totalCostLbl.TabIndex = 10;
+            this.totalCostLbl.Text = "Total Cost";
+            // 
+            // laborCostLbl
+            // 
+            this.laborCostLbl.AutoSize = true;
+            this.laborCostLbl.Location = new System.Drawing.Point(57, 109);
+            this.laborCostLbl.Name = "laborCostLbl";
+            this.laborCostLbl.Size = new System.Drawing.Size(58, 13);
+            this.laborCostLbl.TabIndex = 9;
+            this.laborCostLbl.Text = "Labor Cost";
+            // 
+            // paddingCostLbl
+            // 
+            this.paddingCostLbl.AutoSize = true;
+            this.paddingCostLbl.Location = new System.Drawing.Point(45, 83);
+            this.paddingCostLbl.Name = "paddingCostLbl";
+            this.paddingCostLbl.Size = new System.Drawing.Size(70, 13);
+            this.paddingCostLbl.TabIndex = 8;
+            this.paddingCostLbl.Text = "Padding Cost";
+            // 
+            // carpetCostLbl
+            // 
+            this.carpetCostLbl.AutoSize = true;
+            this.carpetCostLbl.Location = new System.Drawing.Point(39, 57);
+            this.carpetCostLbl.Name = "carpetCostLbl";
+            this.carpetCostLbl.Size = new System.Drawing.Size(76, 13);
+            this.carpetCostLbl.TabIndex = 7;
+            this.carpetCostLbl.Text = "Carpeting Cost";
+            // 
+            // yardsNeededLbl
+            // 
+            this.yardsNeededLbl.AutoSize = true;
+            this.yardsNeededLbl.Location = new System.Drawing.Point(21, 31);
+            this.yardsNeededLbl.Name = "yardsNeededLbl";
+            this.yardsNeededLbl.Size = new System.Drawing.Size(94, 13);
+            this.yardsNeededLbl.TabIndex = 6;
+            this.yardsNeededLbl.Text = "Sq. Yards Needed";
+            // 
+            // carpetEstLbl
+            // 
+            this.carpetEstLbl.AutoSize = true;
+            this.carpetEstLbl.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carpetEstLbl.Location = new System.Drawing.Point(12, 9);
+            this.carpetEstLbl.Name = "carpetEstLbl";
+            this.carpetEstLbl.Size = new System.Drawing.Size(294, 29);
+            this.carpetEstLbl.TabIndex = 12;
+            this.carpetEstLbl.Text = "Carpet Pricing Estimator";
+            // 
+            // firstRoomYes
+            // 
+            this.firstRoomYes.AutoSize = true;
+            this.firstRoomYes.Location = new System.Drawing.Point(128, 183);
+            this.firstRoomYes.Name = "firstRoomYes";
+            this.firstRoomYes.Size = new System.Drawing.Size(43, 17);
+            this.firstRoomYes.TabIndex = 13;
+            this.firstRoomYes.TabStop = true;
+            this.firstRoomYes.Text = "Yes";
+            this.firstRoomYes.UseVisualStyleBackColor = true;
+            // 
+            // firstRoomNo
+            // 
+            this.firstRoomNo.AutoSize = true;
+            this.firstRoomNo.Location = new System.Drawing.Point(189, 183);
+            this.firstRoomNo.Name = "firstRoomNo";
+            this.firstRoomNo.Size = new System.Drawing.Size(39, 17);
+            this.firstRoomNo.TabIndex = 14;
+            this.firstRoomNo.TabStop = true;
+            this.firstRoomNo.Text = "No";
+            this.firstRoomNo.UseVisualStyleBackColor = true;
+            // 
+            // totalCalcLbl
+            // 
+            this.totalCalcLbl.AutoSize = true;
+            this.totalCalcLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalCalcLbl.Location = new System.Drawing.Point(140, 135);
+            this.totalCalcLbl.Name = "totalCalcLbl";
+            this.totalCalcLbl.Size = new System.Drawing.Size(39, 13);
+            this.totalCalcLbl.TabIndex = 19;
+            this.totalCalcLbl.Text = "$0.00";
+            // 
+            // laborCalcLbl
+            // 
+            this.laborCalcLbl.AutoSize = true;
+            this.laborCalcLbl.Location = new System.Drawing.Point(140, 109);
+            this.laborCalcLbl.Name = "laborCalcLbl";
+            this.laborCalcLbl.Size = new System.Drawing.Size(34, 13);
+            this.laborCalcLbl.TabIndex = 18;
+            this.laborCalcLbl.Text = "$0.00";
+            // 
+            // paddingCalcLbl
+            // 
+            this.paddingCalcLbl.AutoSize = true;
+            this.paddingCalcLbl.Location = new System.Drawing.Point(140, 83);
+            this.paddingCalcLbl.Name = "paddingCalcLbl";
+            this.paddingCalcLbl.Size = new System.Drawing.Size(34, 13);
+            this.paddingCalcLbl.TabIndex = 17;
+            this.paddingCalcLbl.Text = "$0.00";
+            // 
+            // carpetCalcLbl
+            // 
+            this.carpetCalcLbl.AutoSize = true;
+            this.carpetCalcLbl.Location = new System.Drawing.Point(140, 57);
+            this.carpetCalcLbl.Name = "carpetCalcLbl";
+            this.carpetCalcLbl.Size = new System.Drawing.Size(34, 13);
+            this.carpetCalcLbl.TabIndex = 16;
+            this.carpetCalcLbl.Text = "$0.00";
+            // 
+            // yardsCalcLbl
+            // 
+            this.yardsCalcLbl.AutoSize = true;
+            this.yardsCalcLbl.Location = new System.Drawing.Point(140, 31);
+            this.yardsCalcLbl.Name = "yardsCalcLbl";
+            this.yardsCalcLbl.Size = new System.Drawing.Size(13, 13);
+            this.yardsCalcLbl.TabIndex = 15;
+            this.yardsCalcLbl.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(128, 117);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 24;
+            // 
+            // costGroup
+            // 
+            this.costGroup.Controls.Add(this.yardsNeededLbl);
+            this.costGroup.Controls.Add(this.paddingCalcLbl);
+            this.costGroup.Controls.Add(this.laborCalcLbl);
+            this.costGroup.Controls.Add(this.carpetCalcLbl);
+            this.costGroup.Controls.Add(this.totalCalcLbl);
+            this.costGroup.Controls.Add(this.carpetCostLbl);
+            this.costGroup.Controls.Add(this.yardsCalcLbl);
+            this.costGroup.Controls.Add(this.paddingCostLbl);
+            this.costGroup.Controls.Add(this.totalCostLbl);
+            this.costGroup.Controls.Add(this.laborCostLbl);
+            this.costGroup.Location = new System.Drawing.Point(269, 52);
+            this.costGroup.Name = "costGroup";
+            this.costGroup.Size = new System.Drawing.Size(237, 164);
+            this.costGroup.TabIndex = 25;
+            this.costGroup.TabStop = false;
+            this.costGroup.Text = "Cost Analysis";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.comboBox1.Location = new System.Drawing.Point(128, 150);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(101, 21);
+            this.comboBox1.TabIndex = 26;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(128, 218);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(101, 23);
+            this.calculateBtn.TabIndex = 27;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            // 
+            // CarpetEstimator
+            // 
+            this.AcceptButton = this.calculateBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 268);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.firstRoomNo);
+            this.Controls.Add(this.firstRoomYes);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.costGroup);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.carpetEstLbl);
+            this.Controls.Add(this.firstRoomLbl);
+            this.Controls.Add(this.paddingLayersLbl);
+            this.Controls.Add(this.carpetRateLbl);
+            this.Controls.Add(this.maxLengthLbl);
+            this.Controls.Add(this.maxWidthLbl);
+            this.Name = "CarpetEstimator";
+            this.Text = "Carpet Estimator";
+            this.costGroup.ResumeLayout(false);
+            this.costGroup.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label maxWidthLbl;
+        private System.Windows.Forms.Label maxLengthLbl;
+        private System.Windows.Forms.Label carpetRateLbl;
+        private System.Windows.Forms.Label paddingLayersLbl;
+        private System.Windows.Forms.Label firstRoomLbl;
+        private System.Windows.Forms.Label totalCostLbl;
+        private System.Windows.Forms.Label laborCostLbl;
+        private System.Windows.Forms.Label paddingCostLbl;
+        private System.Windows.Forms.Label carpetCostLbl;
+        private System.Windows.Forms.Label yardsNeededLbl;
+        private System.Windows.Forms.Label carpetEstLbl;
+        private System.Windows.Forms.RadioButton firstRoomYes;
+        private System.Windows.Forms.RadioButton firstRoomNo;
+        private System.Windows.Forms.Label totalCalcLbl;
+        private System.Windows.Forms.Label laborCalcLbl;
+        private System.Windows.Forms.Label paddingCalcLbl;
+        private System.Windows.Forms.Label carpetCalcLbl;
+        private System.Windows.Forms.Label yardsCalcLbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox costGroup;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button calculateBtn;
+    }
+}
+
