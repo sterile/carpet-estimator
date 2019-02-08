d580bc29dfd5c8bf1ddbea033789ade276aa5e48
Brandon Berney <brandonberney@protonmail.com>
2019-02-08 11:52:57 -0500

    app: present logic

--- carpet-estimator/Form1.cs
+++ carpet-estimator/Form1.cs
@@ -29,26 +29,61 @@ namespace carpet_estimator
 
         private void calculateBtn_Click(object sender, EventArgs e)
         {
-            const decimal FIRST_ROOM_BONUS = 100.00M, // The additional cost incurred if this is the first room of a project.
-                LABOR_COST = 4.50M,                   // The labor rate per square yard of carpet installed.
-                PADDING_COST = 2.75M;                 // The price we charge per square yard of padding.
+            const double FIRST_ROOM_BONUS = 100.00, // The additional cost incurred if this is the first room of a project.
+                LABOR_RATE = 4.50,                  // The labor rate per square yard of carpet installed.
+                PADDING_COST = 2.75;                // The price we charge per square yard of padding.
 
-            const double WASTE_ESTIMATE = 0.10; // The bonus amt. of carpet we'll purchase to factor in wastes
+            const double WASTE_ESTIMATE = 1.10; // The bonus amt. of carpet we'll purchase to factor in wastes
 
             const int FEET_TO_YARDS = 9; // 9 feet is equal to 1 square yard.
 
-            decimal carpetCost, // The cost per square yard of carpet
-                paddingCost,    // The cost per square yard of padding
-                laborCost,      // The cost of labor
-                totalCost;      // The total calculated cost
+            double carpetCharge, // The cost per square yard of carpet
+                paddingCharge,   // The cost per square yard of padding
+                laborCharge,     // The cost of labor
+                totalCharge;     // The total calculated cost
 
-            double area, // The calculated area of the room
-                width,   // The max width of the room
-                length,  // The max length of the room
-                price;   // The price per square yard of carpet
+            double area,     // The calculated area of the room
+                width,       // The max width of the room
+                length,      // The max length of the room
+                carpetRate; // The price per square yard of carpet
 
             int layers,    // The number of layers of padding we'll install
                 firstRoom; // Holds whether or not this is the first room (normally I'd use a bool for this but I don't want to lose points)
+
+
+            // Grab inputs provided by the user
+
+            width = double.Parse(widthTextBox.Text);
+            length = double.Parse(lengthTextBox.Text);
+            carpetRate = double.Parse(cptPriceTextBox.Text);
+
+            layers = int.Parse(paddingSelector.Text);
+
+            if (firstRoomYes.Checked)
+            {
+                firstRoom = 1;
+            }
+            else
+            {
+                firstRoom = 0;
+            }
+
+            // Perform calculations
+
+            area = width * length;
+            area /= FEET_TO_YARDS;
+
+            carpetCharge = area * carpetRate * WASTE_ESTIMATE;
+            paddingCharge = layers * area * PADDING_COST * WASTE_ESTIMATE;
+            laborCharge = area * LABOR_RATE;
+            totalCharge = carpetCharge + paddingCharge + laborCharge + (FIRST_ROOM_BONUS * firstRoom);
+
+            // Present results
+            yardsCalcLbl.Text = area.ToString("F1");
+            carpetCalcLbl.Text = carpetCharge.ToString("C");
+            paddingCalcLbl.Text = paddingCharge.ToString("C");
+            laborCalcLbl.Text = laborCharge.ToString("C");
+            totalCalcLbl.Text = totalCharge.ToString("C");
         }
     }
 }
